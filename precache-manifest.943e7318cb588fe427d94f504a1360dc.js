0d6352980f432169ef68b7b8d539391e547ceb4b
Danilo Silva <danilo.silva@selia.local>
2019-09-26 16:46:06 -0300

    Updates

---
+++ precache-manifest.943e7318cb588fe427d94f504a1360dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068cf78f03969d9d8208cefb32a2db03",
+    "url": "/react-aplication/index.html"
+  },
+  {
+    "revision": "fc0305f2e18be8096f86",
+    "url": "/react-aplication/static/css/main.5e5cec5f.chunk.css"
+  },
+  {
+    "revision": "77a06fbd590ec4f0b336",
+    "url": "/react-aplication/static/js/2.501d10d2.chunk.js"
+  },
+  {
+    "revision": "fc0305f2e18be8096f86",
+    "url": "/react-aplication/static/js/main.e6b3ce02.chunk.js"
+  },
+  {
+    "revision": "176574c39cc38abacb71",
+    "url": "/react-aplication/static/js/runtime-main.b0ef055a.js"
+  }
+]);
